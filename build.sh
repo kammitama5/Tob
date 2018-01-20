3814a4691a32d8349d3be8a7e2cb8ce0d6109cbd
JRock007 <JRock007@users.noreply.github.com>
2018-01-20 14:04:56 +0100

    Updated dependencies

--- build.sh
+++ build.sh
@@ -4,33 +4,33 @@ REPOROOT=$(pwd)
 DEPENDIR="${REPOROOT}/dependencies"
 mkdir -p "${DEPENDIR}"
 
-echo "Building Tor.framework with Carthage…"
+echo "Building Tor.framework and Iobfs4proxy.framework with Carthage..."
 cd "${REPOROOT}"
 carthage update --platform iOS --use-submodules --verbose
 
-cp "${REPOROOT}/Carthage/Checkouts/Tor.framework/Tor/tor/src/config/geoip" "${DEPENDIR}/geoip"
-cp "${REPOROOT}/Carthage/Checkouts/Tor.framework/Tor/tor/src/config/geoip6" "${DEPENDIR}/geoip6"
+# cp "${REPOROOT}/Carthage/Checkouts/Tor.framework/Tor/tor/src/config/geoip" "${DEPENDIR}/geoip"
+# cp "${REPOROOT}/Carthage/Checkouts/Tor.framework/Tor/tor/src/config/geoip6" "${DEPENDIR}/geoip6"
 cp -R "${REPOROOT}/Carthage/Build/iOS/Tor.framework" "${DEPENDIR}/Tor.framework"
+cp -R "${REPOROOT}/Carthage/Build/iOS/Iobfs4proxy.framework" "${DEPENDIR}/Iobfs4proxy.framework"
 
 BUILDDIR="${REPOROOT}/build"
 mkdir -p "${BUILDDIR}"
 
 SRCDIR="${BUILDDIR}/src"
 mkdir -p "${SRCDIR}"
-
-echo $'\n\nCloning https://github.com/mtigas/iObfs.git…'
 cd "${SRCDIR}"
-rm -fr "${SRCDIR}/iObfs"
-git clone https://github.com/mtigas/iObfs.git
 
-echo $'\n\nBuilding iObfs.framework…'
-cd ./iObfs
-./build.sh
+echo $'\nDownloading geoip file from https://github.com/torproject/tor...'
+curl -o geoip https://raw.githubusercontent.com/torproject/tor/master/src/config/geoip
+cp "${SRCDIR}/geoip" "${DEPENDIR}/geoip"
 
-cp -R "${SRCDIR}/iObfs/Iobfs4proxy.framework" "${DEPENDIR}/Iobfs4proxy.framework"
+echo $'\nDownloading geoip6 file from https://github.com/torproject/tor...'
+curl -o geoip6 https://raw.githubusercontent.com/torproject/tor/master/src/config/geoip6
+cp "${SRCDIR}/geoip6" "${DEPENDIR}/geoip6"
 
-echo $'\n\nCleaning up…'
+echo $'\nCleaning up...'
 cd "${REPOROOT}"
+rm "Cartfile.resolved"
 rm -rf "${BUILDDIR}"
 rm -rf "${REPOROOT}/Carthage"
 
